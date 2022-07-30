caeef554b08e43a3ad3fff14726610ec7f427864
QuanyiLi <quanyili0057@gmail.com>
2022-07-30 11:24:56 +0800

    ignore

--- legged_gym/scripts/model_to_numpy.py
+++ legged_gym/scripts/model_to_numpy.py
@@ -2,10 +2,10 @@ import torch as th
 import numpy as np
 
 if __name__=="__main__":
-    path = "/home/quanyi/legged_gym/logs/rough_cassie/Jul28_17-15-27_/model_1500.pt"
+    path = "/home/quanyi/neurips/anymal_tanh/model_1500.pt"
     weights = th.load(path)["model_state_dict"]
     ret = {}
     for layer, weight in weights.items():
         ret[layer] = weight.detach().cpu().numpy()
-    np.savez_compressed("cassie", **ret)
+    np.savez_compressed("anymal_tanh", **ret)
     print("model is converted and saved!")
